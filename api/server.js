f5aaebccd90c61e08e66a3d9a94e3744e9a58755
publiminal <publiminal@gmail.com>
2022-05-20 16:08:45 -0400

    init

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+module.exports = server;
+
+const db = require('../data/dbConfig')
